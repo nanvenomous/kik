a6db10898cb85dd2c0e01ab2089c2191510dfc51
Matthew Garelli <matthew.garelli@gm.com>
2021-01-22 15:36:35 -0700

    MG | added logging in color

--- kik.go
+++ kik.go
@@ -18,3 +18,35 @@ func LogIf(err error) {
 		fmt.Println(err)
 	}
 }
+
+var (
+	nc     string
+	red    string
+	green  string
+	yellow string
+	blue   string
+	purple string
+	cyan   string
+	white  string
+)
+
+func init() {
+	nc = "\033[0m"
+	red = "\033[31m"
+	green = "\033[32m"
+	yellow = "\033[33m"
+	blue = "\033[34m"
+	purple = "\033[35m"
+	cyan = "\033[36m"
+	white = "\033[37m"
+}
+
+// Red prints red
+func Red(msg string) {
+	fmt.Println(string(red), msg, string(nc))
+}
+
+// Green prints green
+func Green(msg string) {
+	fmt.Println(string(green), msg, string(nc))
+}
